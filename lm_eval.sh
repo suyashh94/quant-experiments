1e237b39d84b82be5f06aa67937c27f5bb4ce850
suyashh94 <harlalkasuyash@gmail.com>
2024-11-13 08:05:37 +0000

    eval bash script added

---
+++ lm_eval.sh
@@ -0,0 +1,7 @@
+lm_eval --model vllm \
+  --model_args pretrained="./Phi-3.5-mini-instruct-W8A16",add_bos_token=true \
+  --tasks lambada_openai,hellaswag,piqa,winogrande,truthfulqa_mc1,openbookqa,gsm8k \
+  --num_fewshot 5 \
+  --limit 20 \
+  --batch_size 'auto' \
+  --output_path 'eval-results/'
